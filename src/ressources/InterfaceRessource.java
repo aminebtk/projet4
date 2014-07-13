ffe7e8231029e2981e6faa41c8abb81652f8b680
aminebtk <btk.amine@gmail.com>
2014-07-13 19:42:20 -0400

    m

--- src/ressources/InterfaceRessource.java
+++ src/ressources/InterfaceRessource.java
@@ -9,7 +9,6 @@ import javax.swing.JScrollPane;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.util.Date;
 
 import javax.swing.JList;
 import javax.swing.ListModel;
@@ -17,12 +16,12 @@ import javax.swing.border.LineBorder;
 
 
 public class InterfaceRessource extends JFrame{
-
+	
 	DefaultListModel<String>   listModel;
 	private JLabel lblPro;
-
+	
 	public InterfaceRessource(){
-
+	
 		listModel = new DefaultListModel<String> ();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
@@ -32,41 +31,40 @@ public class InterfaceRessource extends JFrame{
 		panel.setBounds(10, 11, 216, 251);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-
+		
 		JLabel label = new JLabel("L'imprimante est utilis\u00E9e par PROCESSUS :  ");
 		label.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		label.setBounds(10, 11, 207, 14);
 		panel.add(label);
-
+		
 		lblPro = new JLabel("PRO");
 		lblPro.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblPro.setForeground(Color.RED);
 		lblPro.setBounds(10, 36, 201, 14);
 		panel.add(lblPro);
-
+		
 		JLabel lblImpression = new JLabel("Historique d'utilisation :");
 		lblImpression.setBounds(10, 61, 157, 14);
 		panel.add(lblImpression);
-
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 95, 196, 156);
 		panel.add(scrollPane);
-
+		
 		JList<String> list = new JList<String>(listModel);
 		scrollPane.setViewportView(list);
 		list.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		setVisible(true);
-
+		
 	}
 
 	public void ajouterReservation(String string) {
-		Date n = new Date();
-		listModel.add(0,string + " - " +  n.toString()  );
-
+		listModel.addElement(string);
 		lblPro.setText(string);
 	}
 
 	public void libererReservation() {
+		
 		lblPro.setText("Aucun");
 	}
 }
