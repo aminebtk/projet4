f0e9b757ff6e0cb99fb0321eac726ba9760a2767
aminebtk <btk.amine@gmail.com>
2014-07-19 16:48:43 -0400

    G

--- src/Util/Cts.java
+++ src/Util/Cts.java
@@ -11,6 +11,9 @@ public class Cts {
 	public static final int PRINT_RESSOURCE = 4;
 	public static final int SUSCRIBE_PROC = 5;
 	public static final int ADD_PROC = 6;
+	public static final String HELD = "HELD";
+	public static final String REALESE = "REALESE";
+	public static final String WAIT = "WAIT";
 	
 	public static final String IP_RESSOURCE = "127.0.0.1";
 	public static final int PORT_RESSOURCE = 8999;
